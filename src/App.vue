aaae27b9a521a0fbc5cdce9e612f8ad90f283796
Akmal Ergashev <tdpu01@gmail.com>
2025-04-11 11:18:22 +0500

    project done

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<script lang="ts" setup>
+import { ref, computed } from "vue";
+import TaskForm from "./components/TaskForm.vue";
+import TasList from "./components/TaskList.vue";
+import FilterButton from "./components/FilterButton.vue";
+
+const tasks = ref<Task[]>([]);
+const message = ref("Tasks App");
+const filter = ref<TaskFilter>("all");
+
+const filteredTask = computed(() => {
+  switch (filter.value) {
+    case "all":
+      return tasks.value;
+    case "done":
+      return tasks.value.filter((task) => task.done);
+    case "todo":
+      return tasks.value.filter((task) => !task.done);
+  }
+  return tasks.value;
+});
+
+function addTask(newTask: string) {
+  tasks.value.push({
+    id: crypto.randomUUID(),
+    title: newTask,
+    done: false,
+  });
+}
+
+const totalDone = computed(() =>
+  tasks.value.reduce((total, task) => (task.done ? total + 1 : total), 0)
+);
+
+function toggleDone(id: string) {
+  const task = tasks.value.find((task) => task.id === id);
+  if (task) {
+    task.done = !task.done;
+  }
+}
+
+function removeTask(id: string) {
+  const index = tasks.value.findIndex((task) => task.id === id);
+  if (index !== -1) {
+    tasks.value.splice(index, 1);
+  }
+}
+
+function setFilter(value: TaskFilter) {
+  filter.value = value;
+}
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+    <TaskForm @add-task="addTask" />
+    <h3 v-if="!tasks.length">Task mavjud emas</h3>
+    <h3 v-else>{{ totalDone }} / {{ tasks.length }} tasks completed</h3>
+    <div v-if="tasks.length" class="button-container">
+      <FilterButton filter="all" @set-filter="setFilter" />
+      <FilterButton filter="todo" @set-filter="setFilter" />
+      <FilterButton filter="done" @set-filter="setFilter" />
+    </div>
+    <TasList
+      :tasks="filteredTask"
+      @toggle-done="toggleDone"
+      @remove-task="removeTask"
+    />
+  </main>
+</template>
+
+<style>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+}
+
+.button-container {
+  display: flex;
+  justify-content: end;
+  gap: 0.5rem;
+}
+</style>
